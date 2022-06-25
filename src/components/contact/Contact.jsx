1fadddf1169110aa2b01b9a91afb8a1c86eca0d2
vivekposwal <vivekposwal170782@gmail.com>
2022-06-25 14:49:46 +0530

    contact section added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,79 @@
 import React from "react";
 import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+import { BsWhatsapp } from "react-icons/bs";
+
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      "service_63oj30h",
+      "template_75tgrvg",
+      form.current,
+      "4DlGb5vd-qvVHxJH3"
+    );
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>vivekabhi4014@gmail.com</h5>
+            <a href="mailto:vivekabhi4014@gmail.com" target="_blank">
+              Send a Message{" "}
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Vivek Poswal</h5>
+            <a href="https://m.me/vivekposwal" target="_blank">
+              Send a Message{" "}
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>991082****</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=9910820973"
+              target="_blank"
+            >
+              Send a Message{" "}
+            </a>
+          </article>
+        </div>
+        {/*END OF CONTACT OPTIONS*/}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows={7}
+            placeholder="Your Message"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
